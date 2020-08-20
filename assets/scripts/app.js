ad11b2162501d05d2e3dd82b715374563e5c1124
Doug Franklin <doug@dougfranklin.com>
2020-08-20 12:32:09 -0400

    14-issue CSS Updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -76,7 +76,7 @@ class ProductItem {
 		prodEl.innerHTML = `
 			<div>
 				<img src="${this.product.imageUrl}" alt="${this.product.title}">
-				<div class="product=item__content">
+				<div class="product-item__content">
 					<h2>${this.product.title}</h2>
 					<h3>\$${this.product.price}</h3>
 					<p>${this.product.description}</p>
